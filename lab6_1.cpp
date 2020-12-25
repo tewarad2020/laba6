02dd31794e1a6eb71db57d24a1f14982f984605a
lab314-34@labeng.local <lab314-34@labeng.local>
2020-12-25 16:42:09 +0700

    change complete

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+/*  change 1.int to double of output and b.
+           2.cout << output << output << \n; to cout << "output" << output << "\n"; 
+           3.outpot to output and count to cout*/
+        
 using namespace std;
 
 int main() {
